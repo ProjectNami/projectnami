e01d6f3c078b981da0191fc86098b985d5574e39
Spencer Cameron <pippercameron@gmail.com>
2013-07-16 16:17:42 -0500

    Added replacement for MySQL WEEK.

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -3681,20 +3681,21 @@ function send_nosniff_header() {
  * @return string
  */
 function _wp_mysql_week( $column ) {
+	global $wpdb;
+
 	switch ( $start_of_week = (int) get_option( 'start_of_week' ) ) {
 	default :
 	case 0 :
-		return "WEEK( $column, 0 )";
+		sqlsrv_query( $wpdb->dbh, "SET DATEFIRST 7" );
+		return "DATEPART( wk, $column )";
 	case 1 :
-		return "WEEK( $column, 1 )";
 	case 2 :
 	case 3 :
 	case 4 :
 	case 5 :
 	case 6 :
-		//return "WEEK( DATE_SUB( $column, INTERVAL $start_of_week DAY ), 0 )";
-		$start_of_week = -1 * $start_of_week;
-		return "WEEK( DATEADD( DAY, $start_of_week, $column ), 0 )";
+		sqlsrv_query( $wpdb->dbh, "SET DATEFIRST $start_of_week" );
+		return "DATEPART( wk, $column )";
 	}
 }
 
