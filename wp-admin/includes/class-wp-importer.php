90ab8fcb74aeb4dd92857ce97b4bed122929b253
pippercameron <pippercameron@gmail.com>
2015-11-18 23:26:24 -0600

    Labeling PN Mods: Round 2

--- wp-admin/includes/class-wp-importer.php
+++ wp-admin/includes/class-wp-importer.php
@@ -29,7 +29,13 @@ class WP_Importer {
 		// Grab all posts in chunks
 		do {
 			$meta_key = $importer_name . '_' . $bid . '_permalink';
+			
+			/*
+			 * PN Mod: Start
+			 * Replace MySQL LIMIT with MSSQL's OFFSET FETCH.
+			 */
 			$sql = $wpdb->prepare( "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = '%s' ORDER BY post_id OFFSET %d ROWS FETCH NEXT %d ROWS ONLY", $meta_key, $offset, $limit );
+			// PN Mod: End
 			$results = $wpdb->get_results( $sql );
 
 			// Increment offset
@@ -96,7 +102,12 @@ class WP_Importer {
 
 		// Grab all comments in chunks
 		do {
+			/*
+			* PN Mod: Start
+			* Replace MySQL LIMIT with MSSQL's OFFSET FETCH.
+			*/
 			$sql = $wpdb->prepare( "SELECT comment_ID, comment_agent FROM $wpdb->comments ORDER BY comment_ID DESC OFFSET %d ROWS FETCH NEXT %d ROWS ONLY", $offset, $limit );
+			// PN Mod: End
 			$results = $wpdb->get_results( $sql );
 
 			// Increment offset
