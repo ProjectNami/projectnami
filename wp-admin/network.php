ee3a9e7125cb2325723aef9bad5d9a908e9cafc2
Patrick Bates <patrick@projectnami.org>
2016-08-31 21:47:31 -0500

    Remove stray

--- wp-admin/network.php
+++ wp-admin/network.php
@@ -28,7 +28,6 @@ if ( is_multisite() ) {
 	if ( ! defined( 'MULTISITE' ) ) {
 		wp_die( __( 'The Network creation panel is not for WordPress MU networks.' ) );
 	}
-	return false;
 }
 
 require_once( dirname( __FILE__ ) . '/includes/network.php' );
