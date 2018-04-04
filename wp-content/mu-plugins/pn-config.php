b2d1035a7a13023f2abaf7f0743294888ce2675a
Spencer Cameron-Morin <pippercameron@gmail.com>
2018-04-04 11:50:35 -0500

    A place to store any miscellaneous config needed for Core or external plugin issues.

---
+++ wp-content/mu-plugins/pn-config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * A place to store any miscellaneous config needed for Core or external plugin issues.
+ */
+
+/*
+ * Stop Jetpack from writing to options with direct DB calls.
+ */
+define( 'JETPACK_DISABLE_RAW_OPTIONS', true );
