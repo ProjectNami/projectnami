bd6bbbe66df8c578474cb410de8f307244aaadc7
Patrick Bates <patrick@cullmanit.com>
2022-07-12 17:14:42 -0500

    Initial merge with WP 6.0.1

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -2078,6 +2078,7 @@ function set_site_transient( $transient, $value, $expiration = 0 ) {
  * does not encompass all settings available in WordPress.
  *
  * @since 4.7.0
+ * @since 6.1.0 The `show_on_front`, `page_on_front`, and `page_for_posts` options were added.
  */
 function register_initial_settings() {
 	register_setting(
@@ -2232,6 +2233,36 @@ function register_initial_settings() {
 			'type'         => 'integer',
 			'description'  => __( 'Blog pages show at most.' ),
 			'default'      => 10,
+		)
+	);
+
+	register_setting(
+		'reading',
+		'show_on_front',
+		array(
+			'show_in_rest' => true,
+			'type'         => 'string',
+			'description'  => __( 'What to show on the front page' ),
+		)
+	);
+
+	register_setting(
+		'reading',
+		'page_on_front',
+		array(
+			'show_in_rest' => true,
+			'type'         => 'integer',
+			'description'  => __( 'The ID of the page that should be displayed on the front page' ),
+		)
+	);
+
+	register_setting(
+		'reading',
+		'page_for_posts',
+		array(
+			'show_in_rest' => true,
+			'type'         => 'integer',
+			'description'  => __( 'The ID of the page that should display the latest posts' ),
 		)
 	);
 
--- wp-includes/pn-version.php
+++ wp-includes/pn-version.php
@@ -1,3 +1,3 @@
 <?php
 
-$pn_version = '3.0.0';
+$pn_version = '3.0.1';
