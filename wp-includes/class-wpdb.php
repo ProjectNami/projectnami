c28c3714a689eb80604bee46baa772ff435d110a
Solomon Rutzky <srutzky@gmail.com>
2023-11-20 23:19:48 -0500

    Update class-wpdb.php to support nvarchar
    Add "N" prefix to quoted string substitution when preparing SQL for execution. This will change string literals to be interpreted as NVARCHAR (i.e. Unicode / UTF-16) instead of an 8-bit encoding (which changes all characters not in the associated code page to "?"s).

--- wp-includes/class-wpdb.php
+++ wp-includes/class-wpdb.php
@@ -1635,7 +1635,7 @@ class wpdb {
 						 */
 						|| ( '' === $format && '%' !== substr( $split_query[ $key - 1 ], -1, 1 ) )
 					) {
-						$placeholder = "'%" . $format . "s'";
+						$placeholder = "N'%" . $format . "s'";
 					}
 				}
 			}
